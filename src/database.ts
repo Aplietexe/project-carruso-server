21f893545b781939569af29831e4b8c06c5fc67d
Aplietexe <pietropalo@icloud.com>
2022-03-10 06:28:23 -0300

    Set up database connection

---
+++ src/database.ts
@@ -0,0 +1,33 @@
+import { type MongoClientOptions, type Db, MongoClient } from "mongodb"
+
+let database: Db | undefined
+
+const connectToDatabase = async () => {
+  const uri = process.env.MONGO_URI
+
+  if (uri === undefined) {
+    console.error("Error connecting to database")
+    return
+  }
+
+  const options = {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    useNewUrlParser: true,
+    // eslint-disable-next-line @typescript-eslint/naming-convention
+    useUnifiedTopology: true,
+  } as MongoClientOptions
+
+  const client = await MongoClient.connect(uri, options)
+
+  database = client.db("carrusodb")
+
+  console.log("Successfully connected to MongoDB.")
+}
+
+const getDatabase = () => {
+  if (!database) throw new Error("Error retrieving database connection")
+
+  return database
+}
+
+export { connectToDatabase, getDatabase }
